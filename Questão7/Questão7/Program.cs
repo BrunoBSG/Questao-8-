2bda81e480a62d7a9478e13322efe38545ba232d
BrunoBSG <bsimoesgoulart@gmail.com>
2025-10-17 17:26:53 -0300

    7

---
+++ Questão7/Questão7/Program.cs
@@ -0,0 +1,25 @@
+﻿int x = 0;
+int numeroinicial, numerofinal = 0;
+Console.WriteLine("Digite o número final:");
+numerofinal = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o número inicial:");
+numeroinicial = int.Parse(Console.ReadLine());
+while (true)
+{
+    if (numeroinicial > numerofinal)
+    {
+        //finaliza
+        return
+        ;
+    }
+    for (int i = 0; i <= 10; i++)
+    {
+        Console.WriteLine($"{numeroinicial}x{i}={numeroinicial * i}");
+
+    }
+    numeroinicial++;
+}
+
+
+
+
